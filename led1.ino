de0cc0c0a68a631d248601c0bb731d47e5f76467
lubna122 <87071821+lubna122@users.noreply.github.com>
2021-07-19 17:30:18 +0500

    Add files via upload

---
+++ led1.ino
@@ -0,0 +1,15 @@
+#define sensorpin A1
+void setup (){
+Serial.begin(9600);
+pinMode(sensorpin, INPUT);
+
+}
+ 
+void loop ()
+{
+
+int val = analogRead(sensorpin);
+ 
+Serial.println(val);
+}
+  // put your setup code here, to run once:
